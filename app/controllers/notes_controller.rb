103b67d6fb5394ab63dca5fe8bfb330ed54a28fc
Mitchell Glazier <glazierm@gmail.com>
2018-11-26 23:08:29 -0700

    added views

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,29 @@
 class NotesController < ApplicationController
+
+  def index
+    @notes = Note.all
+  end
+
+  def show
+    @note = Note.find(params[:id])
+  end
+
+  def edit
+    @note = Note.find(params[:id])
+  end
+
+  def create
+    @note = Note.create(note_params)
+    if @note.save
+      redirect_to root_path
+    else
+      render: :create
+    end
+  end
+
+  def destroy
+    @note.destroy
+    redirect_to root_path
+  end
+
 end
